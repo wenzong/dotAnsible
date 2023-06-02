0c78da38feee7516e43c9c70360fe37ec1562c61
wenzong <ahngzwz_it@live.com>
2023-06-02 23:34:28 +0800

    Update terraform

--- terraform/README.md
+++ terraform/README.md
@@ -1,26 +1,30 @@
+## Init
+
 ```sh
-cat << EOF > versions.tf
-terraform {
-  required_providers {
-    tencentcloud = {
-      source = "tencentcloudstack/tencentcloud"
-    }
-  }
-}
-EOF
-
-cat << EOF > main.tf
-provider "tencentcloud" {
-  # TENCENTCLOUD_SECRET_ID
-  # TENCENTCLOUD_SECRET_KEY
-  region     = "ap-guangzhou"
-}
-EOF
-
 cat <<EOF >>$HOME/.bash_secret
 export TENCENTCLOUD_SECRET_ID=
 export TENCENTCLOUD_SECRET_KEY=
 EOF
 
 terraform init
+```
+
+## Import
+
+```
+$ rm terraform.tfstate
+$ terraform plan
+Changes to Outputs:
+  + cmd = "terraform import tencentcloud_dnspod_record.record {{ domain }}#{{ record_id }}"
+
+$ terraform import tencentcloud_dnspod_record.record {{ domain }}#{{ record_id }}
+```
+
+## Update
+
+Update record value in main.tf
+
+```
+$ terraform plan
+$ terraform apply
 ```
--- terraform/main.tf
+++ terraform/main.tf
@@ -21,14 +21,15 @@ output "cmd" {
     value = "terraform import tencentcloud_dnspod_record.record ${var.domain}#${data.tencentcloud_dnspod_records.record.result[0].record_id}"
 }
 
-resource "terraform_data" "cluster" {
-    provisioner "local-exec" {
-        command = "terraform import tencentcloud_dnspod_record.record ${var.domain}#${data.tencentcloud_dnspod_records.record.result[0].record_id}"
-    }
-}
+# resource "terraform_data" "cluster" {
+#     provisioner "local-exec" {
+#         command = "terraform import tencentcloud_dnspod_record.record ${var.domain}#${data.tencentcloud_dnspod_records.record.result[0].record_id}"
+#     }
+# }
 
 resource "tencentcloud_dnspod_record"  "record" {
     domain = var.domain
+    sub_domain = var.subdomain
     value = data.tencentcloud_dnspod_records.record.result[0].value
     record_type = data.tencentcloud_dnspod_records.record.result[0].type
     record_line = data.tencentcloud_dnspod_records.record.result[0].line
